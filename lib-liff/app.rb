0f57484d07724bfaaec55f2e8c8883cf061b7460
s0ralit <s0ra_kanda@icloud.com>
2018-10-03 21:43:14 +0900

    エラーハンドリングを追加

--- lib-liff/app.rb
+++ lib-liff/app.rb
@@ -7,24 +7,37 @@ class LINEFrontEndFramework
         @CHANNEL_ACCESS_TOKEN = channel_access_token
     end
 
-    def add_liff(view_type, view_url)
-        uri = URI.parse(Config::HOST)
-        request = Net::HTTP::Post.new(uri)
-        request.content_type = "application/json"
-        request["Authorization"] = "Bearer #{@CHANNEL_ACCESS_TOKEN}"
-        request.body = JSON.dump({
-            "view" => {
-                "type" => view_type,
-                "url" => view_url
+    def add_liff(view_type=nil, view_url=nil)
+        if view_type == nil and view_url == nil
+            raise("You must enter view_type and view_url")
+        elsif view_type == nil
+            raise("You must enter view_type")
+        elsif view_url == nil
+            raise("You must enter view_url")
+        else
+            uri = URI.parse(Config::HOST)
+            request = Net::HTTP::Post.new(uri)
+            request.content_type = "application/json"
+            request["Authorization"] = "Bearer #{@CHANNEL_ACCESS_TOKEN}"
+            request.body = JSON.dump({
+                "view" => {
+                    "type" => view_type,
+                    "url" => view_url
+                }
+            })
+            req_options = {
+                use_ssl: uri.scheme == "https",
             }
-        })
-        req_options = {
-            use_ssl: uri.scheme == "https",
-        }
-        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-            http.request(request)
+            response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+                http.request(request)
+            end
+            if response.code == "200"
+                return "line://app/#{JSON.load(response.body)["liffId"]}"
+            elsif response.code == "400"
+                raise("400 Error\n・The request contains an invalid value.\n・The maximum number of LIFF applications that can be added to the channel has been reached.")
+            elsif response.code == "401"
+                raise("401 Error\nCertification failed.")
+            end
         end
-        return "line://app/#{JSON.load(response.body)["liffId"]}"
     end
-    
 end
