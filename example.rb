f81779c09d615849e136cc9258bb4016eaf002fe
sora <43514335+litsora@users.noreply.github.com>
2019-04-20 08:59:55 +0900

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,4 +1,5 @@
 require './lib-liff/liff_api'
 liff = LIFF.new("your_channel_access_token")
-puts liff.update_liff("view type", "https://example.com")
-puts liff.delete_liff("your_liffId")
+puts liff.add_liff("view_type", "view_url") # => "line://app/{liffid}"
+puts liff.update_liff("view type", "view_url") # => "Success"
+puts liff.delete_liff("liffId") # => "Success\n[View type]"view_type"\n[View url]"view_url"
